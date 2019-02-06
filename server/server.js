9d8a5a8572be1493afdbca0e3e83a49c8efff755
mannyanebi <anebiemmanuel@gmail.com>
2019-02-06 12:25:06 +0100

    Added comments to server.js and played with test-promises.js

--- server/server.js
+++ server/server.js
@@ -57,20 +57,25 @@ app.post('/api/address', function (req, res) {
      // note that the body property of req is now parsed
     // into a JS object from a JSON format using body-parser
     let address = req.body.address;
+    //concatenates the ss parameter needed for the [time] parameter
     let datetime = req.body.datetime + ":00";
     
-    console.log('address:', address);
+    //2019-02-06T06:00:00
+    //Geocoding user's address to get positional coordinates
     console.log('Fetching your address positional coordinates');
     axios.get(`https://geocoder.api.here.com/6.2/geocode.json?app_id=KVuwMjndNDZxztDGks8Z&app_code=BFTTB1Hlw9eKyNw4zWve6Q&searchtext=${address}`)
         .then(function (response) {
             console.log('Got response from geocoder, fetching location weather information');
+            //returns the positional coordinates for the next then call to make a GET request
             return response.data.Response.View[0].Result[0].Location.DisplayPosition;
         }).then(function (coordinates) {
             console.log(`https://api.darksky.net/forecast/01f9e6361372fc3b78310e171d41181a/${coordinates.Latitude},${coordinates.Longitude},${datetime}?exclude=hourly,currently,flags`)
+            //makes a GET request with the positional request to darksky api and then returns the result to the next then call
             return axios.get(`https://api.darksky.net/forecast/01f9e6361372fc3b78310e171d41181a/${coordinates.Latitude},${coordinates.Longitude},${datetime}?exclude=hourly,currently,flags`)
         })
         .then(function (weather_response) {
             console.log(JSON.stringify(weather_response.data.daily.data[0].temperatureMax, null, 2));
+            //sends this response to the back to the client
             res.send(JSON.stringify(weather_response.data.daily.data[0].temperatureMax, null, 2));
          })
         .catch(function (err) {
